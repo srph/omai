9239eff42f829372b2547614e485d6c7186dc03a
srph <seraphipod@gmail.com>
2015-02-28 20:49:11 +0800

    Added App component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+"use strict";
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        Yeh?
+      </div>
+    );
+  }
+});
+
+// Yolo pls swag.
+React.render(<App />, document.body);
