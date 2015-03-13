0331011b1831bc44ec1595a4b8642a71a5dee5de
srph <seraphipod@gmail.com>
2015-03-13 20:20:10 +0800

    Removed unnecessary styling (and media query).

--- src/components/Profile/Description/Description.jsx
+++ src/components/Profile/Description/Description.jsx
@@ -24,29 +24,31 @@ var Description = React.createClass({
     var {active, activationHandler} = this.props;
 
     var styles = {
-      // The container div
+      // The container element
       'box': {
+        // Set to relative since our caret (absolute positioned)
+        // mainly depends on this element container.
         position: 'relative',
+        // Spacing for the DP, Title
         marginTop: 35,
+        // Center teh shit
         marginLeft: 'auto',
         marginRight: 'auto',
 
         padding: '0px 10px',
+        // Add swag
         backgroundColor: '#fff',
         boxShadow: '0 1px 0 rgba(0, 0, 0, 0.1)',
         borderRadius: 6,
-
-        lineHeight: '1.5',
-
-        mediaQueries: [{
-          sm: {
-            width: '100%'
-          }
-        }]
       },
 
       // Description text
-      'text': { paddingTop: 20, paddingBottom: 20 },
+      'text': {
+        // Space swag!
+        paddingTop: 20, paddingBottom: 20,
+        // Improve for readability
+        lineHeight: '1.5'
+      },
     };
 
     return (
